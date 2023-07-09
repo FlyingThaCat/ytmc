e4ca8c83c67e7afe5e7ab1d14789018c2b6234d3
John <95353217+FlyingThaCat@users.noreply.github.com>
2023-07-09 17:21:54 +0700

    [featured on, related artist, artist bio] UNTESTED

--- lib/pages/ArtistPage.dart
+++ lib/pages/ArtistPage.dart
@@ -848,6 +848,205 @@ class _ArtistPageState extends State<ArtistPage> {
                       ),
                     ),
                   
+                    // Featured channels
+                    const Padding(
+                      padding: EdgeInsets.only(top: 12, bottom: 2.5, left: 16),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Featured on',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Featured channels Carousel
+                    SizedBox(
+                      height: 200,
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ListView.builder(
+                          padding: const EdgeInsets.only(top: 2.5),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: (response['featuredChannels']
+                                      as Map<String, dynamic>?)?['contents']
+                                  ?.length ??
+                              0,
+                          itemBuilder: (context, index) {
+                            final featuredChannelsList = (response['featuredChannels']
+                                as Map<String, dynamic>?)?['contents'];
+                            final featuredChannelsListTitle = featuredChannelsList?[index]
+                                    ['musicTwoRowItemRenderer']['title']['runs']
+                                [0]['text'];
+                            final featuredChannelsListThumbnail = featuredChannelsList?[index]
+                                            ['musicTwoRowItemRenderer']
+                                        ['thumbnailRenderer']
+                                    ['musicThumbnailRenderer']['thumbnail']
+                                ['thumbnails'][0]['url'];
+                            return Container(
+                              width: 225,
+                              margin: EdgeInsets.only(
+                                  top: 12, bottom: 2.5, left: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // add rounded corners to image and cover
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Container(
+                                      width: 225,
+                                      height: 127,
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        // add background color to list tile
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                            featuredChannelsListThumbnail
+                                                    ?.toString() ??
+                                                '',
+                                          ),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: RichText(
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          text: TextSpan(
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                text: featuredChannelsListTitle
+                                                        ?.toString() ??
+                                                    '',
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+
+                    // Related artists
+                    const Padding(
+                      padding: EdgeInsets.only(top: 12, bottom: 2.5, left: 16),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Related artists',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Related artists Carousel
+                    SizedBox(
+                      height: 200,
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ListView.builder(
+                          padding: const EdgeInsets.only(top: 2.5),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: (response['relatedArtists']
+                                      as Map<String, dynamic>?)?['contents']
+                                  ?.length ??
+                              0,
+                          itemBuilder: (context, index) {
+                            final relatedArtistsList = (response['relatedArtists']
+                                as Map<String, dynamic>?)?['contents'];
+                            final relatedArtistsListTitle = relatedArtistsList?[index]
+                                    ['musicTwoRowItemRenderer']['title']['runs']
+                                [0]['text'];
+                            final relatedArtistsListThumbnail = relatedArtistsList?[index]
+                                            ['musicTwoRowItemRenderer']
+                                        ['thumbnailRenderer']
+                                    ['musicThumbnailRenderer']['thumbnail']
+                                ['thumbnails'][0]['url'];
+                            return Container(
+                              width: 225,
+                              margin: EdgeInsets.only(
+                                  top: 12, bottom: 2.5, left: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // add rounded corners to image and cover
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Container(
+                                      width: 225,
+                                      height: 127,
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        // add background color to list tile
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                            relatedArtistsListThumbnail
+                                                    ?.toString() ??
+                                                '',
+                                          ),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: RichText(
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          text: TextSpan(
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                text: relatedArtistsListTitle
+                                                        ?.toString() ??
+                                                    '',
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
