d353c83d2e14e71508d227f5b6b4f0a3547c792e
John <95353217+FlyingThaCat@users.noreply.github.com>
2023-07-13 01:12:55 +0700

    add code to artist bio component

---
+++ lib/components/artist_bio.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class ArtistBioComponent extends StatelessWidget {
+  final Map<String, dynamic> artistBio;
+  final Map<String, dynamic> thumbails;
+
+  const ArtistBioComponent({super.key, required this.artistBio, required this.thumbails});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Artist bio
+        const Padding(
+          padding: EdgeInsets.only(top: 12, bottom: 2.5, left: 16),
+          child: Row(
+            children: [
+              Text(
+                'Artist bio',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // Artist bio
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(left: 10, top: 2.5, right: 10),
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            // show image with rounded corners using original image
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(
+                    thumbails['originThumbnail'],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 300,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.8),
+                          ],
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                artistBio['artistName'],
+                                style: const TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text(
+                                          artistBio['artistName'],
+                                        ),
+                                        content: Text(
+                                          artistBio['artistBioContent'],
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Close'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_forward_rounded,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            artistBio['artistBioViewCounter'],
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            artistBio['artistBioContentShort'],
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/pages/ArtistPage.dart
+++ lib/pages/ArtistPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:ytmc/components/artist_bio.dart';
 import 'package:ytmc/components/featured_playlists.dart';
 import 'package:ytmc/components/singles.dart';
 import 'package:ytmc/components/videos.dart';
@@ -148,138 +149,7 @@ class _ArtistPageState extends State<ArtistPage> {
                     VideosComponent(videos: videos),
                     FeaturedPlaylistsComponent(featuredPlaylists: featuredPlaylists),
                     RelatedArtistsComponent(relatedArtists: relatedArtists),
-                    
-                    
-
-                    
-
-                    // Artist bio
-                    const Padding(
-                      padding: EdgeInsets.only(top: 12, bottom: 2.5, left: 16),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Artist bio',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-
-                    // Artist bio
-                    Container(
-                      width: double.infinity,
-                      margin:
-                          const EdgeInsets.only(left: 10, top: 2.5, right: 10),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)),
-                        // show image with rounded corners using original image
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Stack(
-                            children: <Widget>[
-                              Image.network(
-                                thumbails['originThumbnail'],
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                                height: 300,
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                child: Container(
-                                  padding: const EdgeInsets.all(16),
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      begin: Alignment.topCenter,
-                                      end: Alignment.bottomCenter,
-                                      colors: [
-                                        Colors.transparent,
-                                        Colors.black.withOpacity(0.8),
-                                      ],
-                                    ),
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text(
-                                            artist['artistName'],
-                                            style: const TextStyle(
-                                              fontSize: 30,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          IconButton(
-                                            onPressed: () {
-                                              showDialog(
-                                                context: context,
-                                                builder:
-                                                    (BuildContext context) {
-                                                  return AlertDialog(
-                                                    title: Text(
-                                                      artist['artistName'],
-                                                    ),
-                                                    content: Text(
-                                                      artist[
-                                                          'artistBioContent'],
-                                                    ),
-                                                    actions: [
-                                                      TextButton(
-                                                        onPressed: () {
-                                                          Navigator.pop(
-                                                              context);
-                                                        },
-                                                        child:
-                                                            const Text('Close'),
-                                                      ),
-                                                    ],
-                                                  );
-                                                },
-                                              );
-                                            },
-                                            icon: const Icon(
-                                              Icons.arrow_forward_rounded,
-                                              color: Colors.white,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      const SizedBox(height: 4),
-                                      Text(
-                                        artist['artistBioViewCounter'],
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 4),
-                                      Text(
-                                        artist['artistBioContentShort'],
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
+                    ArtistBioComponent(artistBio: artist, thumbails: thumbails),
                   ],
                 ),
               ),
